9d7b8d1e5af2674ea26e03e9dc5437549cde0847
ale159156 <alee159156@gmail.com>
2026-07-03 21:09:16 +0000

    .TS

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import { logger } from "./lib/logger.js";
+import app from "./app.";
+import { logger } from "./lib/logger.";
 import { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags } from "discord.js";
 import { GoogleGenAI } from "@google/genai";
 import { google } from "googleapis";
